954fd95c41620fcefe14561a7638b6144c5eed0e
Matias Olivera <moliverafreire@gmail.com>
2023-06-26 20:25:51 -0600

    feat: initialize both wallet reference implementations

--- packages/examples/extension/src/content/window.ts
+++ packages/examples/extension/src/content/window.ts
@@ -1,13 +1,27 @@
-import { initialize } from '@exodus/bitcoin-wallet-standard-sats-connect';
+import { initialize as initializeSatsConnect } from '@exodus/bitcoin-wallet-standard-sats-connect';
+import { initialize as initializeSatoshi } from '@exodus/bitcoin-wallet-standard-satoshi';
 
 import { createWindowRPC } from '../rpc';
 import { BitcoinProvider } from './provider';
 
 const rpc = createWindowRPC(window);
+
 const provider = new BitcoinProvider(rpc);
-initialize(provider);
 
 // Temporary to allow debugging in the browser.
 Object.defineProperty(window, 'BitcoinProvider', {
     value: provider,
 });
+
+/**
+ * Initialize the standard wallet using both reference implementations:
+ *
+ * A. Satoshi Wallet: A wallet written with a Wallet Standard compatible API (recommended), and
+ * B. Sats Connect Wallet: A wallet wrapping the Sats Connect API with a Wallet Standard compatible API.
+ */
+
+// A. Satoshi Wallet
+initializeSatoshi(rpc);
+
+// B. Sats Connect Wallet
+initializeSatsConnect(provider);
