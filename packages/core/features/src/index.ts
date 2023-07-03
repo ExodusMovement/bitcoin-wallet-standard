dd99f91fba48464a13d528ab609aff758d92450a
Kirill Bulgakov <kbulgakov@exodus.io>
2023-07-03 19:06:38 +0300

    chore: export * from `signTransaction.js`

--- packages/core/features/src/index.ts
+++ packages/core/features/src/index.ts
@@ -1,10 +1,12 @@
 import type { WalletWithFeatures } from '@wallet-standard/base';
 import type { BitcoinConnectFeature } from './connect.js';
+import type { BitcoinSignTransactionFeature } from './signTransaction.js';
 
 /** Type of all Bitcoin features. */
-export type BitcoinFeatures = BitcoinConnectFeature;
+export type BitcoinFeatures = BitcoinConnectFeature & BitcoinSignTransactionFeature;
 
 /** Wallet with Bitcoin features. */
 export type WalletWithBitcoinFeatures = WalletWithFeatures<BitcoinFeatures>;
 
 export * from './connect.js';
+export * from './signTransaction.js';
