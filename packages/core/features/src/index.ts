4bc4572d09a5330750ba2a1c86c9411d36064608
tian000 <14320466+tian000@users.noreply.github.com>
2023-09-06 09:42:00 -0700

    bitcoin:signMessage

--- packages/core/features/src/index.ts
+++ packages/core/features/src/index.ts
@@ -2,12 +2,14 @@ import type { WalletWithFeatures } from '@wallet-standard/base';
 import type { BitcoinConnectFeature } from './connect.js';
 import type { BitcoinSignAndSendTransactionFeature } from './signAndSendTransaction.js';
 import type { BitcoinSignTransactionFeature } from './signTransaction.js';
+import type { BitcoinSignMessageFeature } from './signMessage.js';
 
 /** Type alias for some or all Bitcoin features. */
 export type BitcoinFeatures =
     | BitcoinConnectFeature
     | BitcoinSignTransactionFeature
-    | BitcoinSignAndSendTransactionFeature;
+    | BitcoinSignAndSendTransactionFeature
+    | BitcoinSignMessageFeature;
 
 /** Wallet with Bitcoin features. */
 export type WalletWithBitcoinFeatures = WalletWithFeatures<BitcoinFeatures>;
@@ -15,3 +17,4 @@ export type WalletWithBitcoinFeatures = WalletWithFeatures<BitcoinFeatures>;
 export * from './connect.js';
 export * from './signTransaction.js';
 export * from './signAndSendTransaction.js';
+export * from './signMessage.js';
