d6e15e2bdc223a4705701ff24b6a12bf62498b07
Matias Olivera <moliverafreire@gmail.com>
2023-09-06 10:49:57 -0600

    feat(core:features): export feature

--- packages/core/features/src/index.ts
+++ packages/core/features/src/index.ts
@@ -1,12 +1,17 @@
 import type { WalletWithFeatures } from '@wallet-standard/base';
 import type { BitcoinConnectFeature } from './connect.js';
+import type { BitcoinSignAndSendTransactionFeature } from './signAndSendTransaction.js';
 import type { BitcoinSignTransactionFeature } from './signTransaction.js';
 
-/** Type of all Bitcoin features. */
-export type BitcoinFeatures = BitcoinConnectFeature & BitcoinSignTransactionFeature;
+/** Type alias for some or all Bitcoin features. */
+export type BitcoinFeatures =
+    | BitcoinConnectFeature
+    | BitcoinSignTransactionFeature
+    | BitcoinSignAndSendTransactionFeature;
 
 /** Wallet with Bitcoin features. */
 export type WalletWithBitcoinFeatures = WalletWithFeatures<BitcoinFeatures>;
 
 export * from './connect.js';
 export * from './signTransaction.js';
+export * from './signAndSendTransaction.js';
